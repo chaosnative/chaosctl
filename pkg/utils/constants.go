854f813608aedf2ddb4be3b8d012a6e94652cb02
Saranya-jena <saranya@chaosnative.com>
2022-04-14 16:53:45 +0530

    Reverted back unecessary changes in constants.go file
    Signed-off-by: Saranya-jena <saranya@chaosnative.com>

--- pkg/utils/constants.go
+++ pkg/utils/constants.go
@@ -1,12 +1,9 @@
 /*
 Copyright © 2021 The LitmusChaos Authors
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -57,10 +54,10 @@ const (
 	ChaosAgentPath = "targets"
 
 	// Graphql server api path
-	GQLAPIPath = "/query"
+	GQLAPIPath = "/api/query"
 
 	// Auth server api path
-	AuthAPIPath = ""
+	AuthAPIPath = "/auth"
 
-	HarnessChaosEngineeringCloudEndpoint = "https://cloud.chaosnative.com"
+	ChaosNativeCloudEndpoint = "https://cloud.chaosnative.com"
 )
